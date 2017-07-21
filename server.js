6e37b1404a01500c8b5624e6975d7f4273eb0ee5
anzcarroll <anzcarroll@gmail.com>
2017-07-21 14:03:42 -0400

    started files and folders

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hbs = require('hbs');
+
+
+const app = express();
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen('port', (req, res) => {
+    console.log("cant wait to not get shit done on" + port);
+})
